b1358f74366f9773c4a3bffddc99ef84fe32871c
Viraj Bhagiya <virajbhagiya@gmail.com>
2024-10-09 15:39:53 +0530

    cors change

--- backend/src/managers/IoManager.ts
+++ backend/src/managers/IoManager.ts
@@ -11,7 +11,7 @@ export class IoManager {
         if (!this.io) {
             const io = new Server(server, {
                 cors: {
-                    origin: "*",
+                    origin: "https://quiz-app-lime-five.vercel.app/",
                     methods: ["GET", "POST"]
                 }
             });
